ba40c7c34ce81e78c93bc9a0d004fc3e0b1f4767
PercivallAkihiko <37913210+PercivallAkihiko@users.noreply.github.com>
2018-04-13 12:51:19 +0200

    Delete session.php

--- index_prova/php/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-
-// Echo session variables that were set on previous page
-    echo "Username: " . $_SESSION["username"] . ".<br>";
-    echo "Rank: " . $_SESSION["rank"] . ". <br>";
-    echo "Not_Match: " . $_SESSION["notmatch"];
-?>
